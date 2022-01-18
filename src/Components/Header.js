c946c23e9a2452f8eb80c45f2db7f3241ae9881c
Edneuza <edneuza.fernandes@gmail.com>
2022-01-17 21:27:16 -0300

    criação de componentes de formulario

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,14 +5,14 @@ import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to="/" aria-label="Dogs - Home">
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login">Login / Criar login</Link>
+        <Link className={styles.login} to="/login">Login / Criar login</Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
---
+++ src/Components/Login/LoginPasswordLost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPasswordLost = () => {
+    return (
+        <div>
+            password lost
+        </div>
+    )
+}
+
+export default LoginPasswordLost
